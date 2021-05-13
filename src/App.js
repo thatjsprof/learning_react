8bff7b3480ab34ca3addef1716203643144c81f8
David Ajayi <davidrums65@gmail.com>
2021-05-13 13:35:42 +0100

    more lessons on component architecture

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super()
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -24,9 +25,8 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        {
+          <input type="search" placeholder="Search Monsters" onChange={(e) => this.setState({ searchField: e.target.value })}/>
           <CardList monsters={this.state.monsters}></CardList>
-        }
       </div>
     )
   }
